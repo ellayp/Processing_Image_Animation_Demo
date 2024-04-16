200a68e4275bf4584d633e54c79c6d5f5212ed74
ellas <ella.yap26@ycdsbk12.ca>
2024-04-16 11:36:25 -0400

    Updated Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,17 +1,32 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+
+  // Declare variables before methods, settings, setup, and draw.
+  PImage imgMeteor; // "img" follows conventions
+
+  // x and y coordinates for meteor
+  float fltMeteorX = 0; // Declares and initializes in the same line
+  float fltMeteorY = 0; // Declares and initializes in the same line
+
+  // Define colours
+  int black = color(0);
+  int white = color(255);
+  int magenta = color(255, 0, 255);
 
   public void settings() {
     size(400, 400);
   }
 
   public void setup() {
-    background(0, 0, 0);
     // load meteor
+    imgMeteor = loadImage("spaceMeteors_003.png");
 
     // resize meteor
-    
+    //imgMeteor.resize(50, 50); // Sets the width and height measures of the image
+    imgMeteor.resize(imgMeteor.width/4, imgMeteor.height/4);
+
     // load missile
 
     // resize missile
@@ -23,8 +38,12 @@ public class Sketch extends PApplet {
   }
 
   public void draw() {
+    background(black);
+    
     // draw meteor and move
-
+    // image (imgMeteor, 0, 0);
+    // image (imgMeteor, fltMeteorX, fltMeteorY);
+    image (imgMeteor, mouseX, mouseY);
 
     // draw missile and move
 
